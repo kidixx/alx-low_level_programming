7c9275a4ebe07f92d69c24307a3ac7221f2bee03
kidixx <imop0316@gmail.com>
2023-02-16 23:58:37 -0800

    put command

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * main - thie is the main function
+ *
+ * Reture - always 0
+ */
+
+int main(void)
+        puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}	
