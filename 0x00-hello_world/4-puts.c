f06008acaf64bf091f616fd2c95f2e0060f786fd
kidixx <imop0316@gmail.com>
2023-02-17 00:54:40 -0800

    betty de like mad

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - this the main function
- *
- * Return: always 0
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
  */
-
 int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
-}	
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+	
