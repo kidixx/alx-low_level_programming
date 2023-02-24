b4cb1cc2cffe829c941455de4d93b9329deb878c
kidixx <imop0316@gmail.com>
2023-02-24 11:47:14 -0800

    number and signnnn

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - Entry point
+ * Return: success
+ */
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a = 48; a < 58; a++)
+	{
+		if (!(a == 50 || a == 52))
+			_putchar(a);
+	}
+	_putchar('\n');
+}
