7b33891614320423cdbe465dd884a817156beb75
kidixx <imop0316@gmail.com>
2023-03-02 01:03:15 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncmp - compares string
+ * @s1: input parameter
+ * @s2: input parameter
+ *
+ * Return: 0 if equal or x is differeent
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0;
+	int x = 0;
+
+	while (s1[a] != '\0' && x == 0)
+	{
+		x = s1[a] - s2[a];
+		a++;
+	}
+	return (x);
+
+}
