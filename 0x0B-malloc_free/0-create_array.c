9de35254dc782f0bef28c76eb2c3314505556713
kidixx <imop0316@gmail.com>
2023-03-14 00:45:39 -0700

    float like a bufferfly, string like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,12 @@ char *create_array(unsigned int size, char c)
 {
 	char *array;
 	unsigned int i = 0;
-	
+
 	if (size == 0)
 		return (NULL);
 
 	array = malloc(size * sizeof(char));
-	
+
 	if (array == NULL)
 		return (NULL);
 
