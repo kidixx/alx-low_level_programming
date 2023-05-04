eb5c660b6d19e296627f1fa1da566a752f3b16ed
kidixx <imop0316@gmail.com>
2023-05-04 05:23:04 -0700

    binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+/*
+ * File_name: 0-binary_to_uint.c
+ * date: 4th of may, 2023
+ * owner: imo prince
+ * Size: nonspecified
+ * Project: alx-low_level_programming
+ * Status: passed.
+ */
+
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: a pointer a string of 0 and 1 chars.
+* Return: If b is NULL or contains chars not 0 or 1 - 0.
+ *         Otherwise - the converted number.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int brny = 0;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		brny = 2 * brny + (b[i] - '0');
+	}
+
+	return (brny);
+}
+
